611e0586869f8180e3fc68ead352264917cf0cb2
Sargin Stas <stassargin@gmail.com>
2019-08-17 17:49:35 +0300

    правки и панель состояния

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Editor
+        Unisharp\Ckeditor\ServiceProvider::class
+
     ],
 
     /*
